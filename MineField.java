67467077ea0ae452bb70770ae94c350d81c55878
Nazar Leonidovich Stelmakh <nazar.stelmakh@wsu.edu>
2016-04-11 12:16:27 -0700

    Day 2 Commit 2: Expose is working. AutoExpose is working.

--- MineField.java
+++ MineField.java
@@ -9,10 +9,12 @@ import javafx.stage.Stage;
 
 public class MineField extends Application {
 
+    Cell[][] matrix;
     private int width;
     private int height;
     private int numOfMines;
-    Cell[][] matrix;
+    private boolean alreadyLost;
+    private int numExposedCells;
 
 
     private class Cell {
@@ -40,6 +42,9 @@ public class MineField extends Application {
             numOfMines = 99;
         }
 
+        numExposedCells = 0;
+        alreadyLost = false;
+
         matrix = new Cell[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width ; j++) {
@@ -57,11 +62,11 @@ public class MineField extends Application {
         return matrix;
     }
 
-    private static MineField setMines(MineField mineField){
-        int w = mineField.width;
-        int h = mineField.height;
+    private MineField setMines(MineField mineField){
+        int w = width;
+        int h = height;
         int n = w*h;
-        int m = mineField.numOfMines;
+        int m = numOfMines;
         int r, c;
 
         for(r = 0; r < h; r++){
@@ -69,24 +74,24 @@ public class MineField extends Application {
                 double p = (double) m / (double) n; // probability of placing mine here
                 double g = Math.random();
                 if (g < p) {
-                    mineField.matrix[r][c].hasMine = true;
+                    matrix[r][c].hasMine = true;
                     m--;
                 }
                 n--;
             }
         }
-        mineField.numOfMines -= m; // numOfMines = 0
+        numOfMines -= m; // numOfMines = 0
 
         for (r = 0; r < h; r++){
             for(c = 0; c < w; c++){
                 int i,j, count = 0;
-                Cell currentCell = mineField.matrix[r][c];
+                Cell currentCell = matrix[r][c];
                 for(j = -1; j <= +1; j++){
                     for(i = -1; i <= +1; i++){
                         if (i == 0 && j == 0) continue;
                         int rr = r+j, cc = c+i;
                         if(rr < 0 || rr >= h || cc < 0 || cc >= w) continue;
-                        Cell neighbor = mineField.matrix[rr][cc];
+                        Cell neighbor = matrix[rr][cc];
                         if(neighbor.hasMine) count++;
                     }
                 }
@@ -118,9 +123,62 @@ public class MineField extends Application {
         return false;
     }
 
-    public int expose(int column, int row){
-
-        return 0;
+    public int expose(int row, int col){
+        if(alreadyLost) return -2;
+        Cell cell = matrix[row][col];
+        if(matrix[row][col].exposed) return -2;
+        cell.exposed = true;
+        numExposedCells++;
+        if(cell.hasMine){
+            alreadyLost = true;
+            return -1;
+        }
+        int n = cell.numSurroundingMines;
+        System.out.println("numSurroundingMines = " + n);
+        if (n == 0){
+            int w = width;
+            int h = height;
+            boolean changed = true;
+            //do{
+            while(changed){
+                //System.out.println("Entered while loop");
+                int rr, cc;
+                changed = false;
+                for(rr = 0; rr < h; rr++){
+                    for(cc = 0; cc < w; cc++){
+                        //System.out.println("Entered Second for Loop");
+                        if(autoExposeCellAt(rr, cc))
+                            changed = true;
+                    }
+                }
+            } //while(changed);
+        }
+        return n;
+    }
+    public boolean autoExposeCellAt(int row, int col){
+        //System.out.println("Entered autoExpose");
+        Cell cell = matrix[row][col];
+        if(!cell.exposed && !cell.hasMine){
+            int w = width;
+            int h = height;
+            int i, j;
+            for(j = -1; j <= +1; j++){
+                for(i = -1; i <= +1; i++){
+                    if (i == 0 && j == 0) continue;
+                    int rr = row+j;
+                    int cc = col+i;
+                    if(rr < 0 || rr >= h || cc < 0 || cc >= w) continue;
+                    Cell neighbor = matrix[rr][cc];
+                    //System.out.println("Reached Here");
+                    if(neighbor.exposed && neighbor.numSurroundingMines == 0){
+                        cell.exposed = true;
+                        numExposedCells++;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public int isExposed(int column, int row){
@@ -141,17 +199,17 @@ public class MineField extends Application {
 
         newMineField.makeField(difficulty);
 
-        for (int i = 0; i < newMineField.height; i++) {
-            for (int j = 0; j < newMineField.width; j++) {
-                System.out.print("[ ] ");
-            }
-            System.out.println();
+//        for (int i = 0; i < newMineField.height; i++) {
+//            for (int j = 0; j < newMineField.width; j++) {
+//                System.out.print("[ ] ");
+//            }
+//            System.out.println();
+//
+//        }
+//        System.out.println();
 
-        }
+        newMineField.setMines(newMineField);
 
-        newMineField = setMines(newMineField);
-
-        System.out.println();
         int mines = 0;
 
         for (int i = 0; i < newMineField.height; i++) {
@@ -160,16 +218,35 @@ public class MineField extends Application {
                     System.out.print("[*] ");
                     mines ++;
                 }
-                else if(newMineField.matrix[i][j].numSurroundingMines > 0){
-                    System.out.print("["+newMineField.matrix[i][j].numSurroundingMines +"] ");
+//                else if(newMineField.matrix[i][j].numSurroundingMines > 0){
+//                    System.out.print("["+newMineField.matrix[i][j].numSurroundingMines +"] ");
+//                }
+                else System.out.print("[ ] ");
+            }
+            System.out.println();
+
+        }
+        //System.out.println("Mines: " + mines);
+        System.out.println();
+
+        int expRow = 1;
+        int expCol = 1;
+        newMineField.expose(expRow, expCol);
+
+        for (int i = 0; i < newMineField.height; i++) {
+            for (int j = 0; j < newMineField.width; j++) {
+                if(newMineField.matrix[i][j].hasMine == true){
+                    System.out.print("[*] ");
+                    mines ++;
+                }
+                else if(newMineField.matrix[i][j].exposed == true){
+                    System.out.print("[O] ");
                 }
                 else System.out.print("[ ] ");
             }
             System.out.println();
 
         }
-        System.out.println("Mines: " + mines);
-
 
     }
 }
